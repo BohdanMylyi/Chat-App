3e8a3e460826c3ac3c6fd781af487b47641239c1
Bohdan Mylyi <mylyibohdan18@gmail.com>
2025-01-09 14:08:57 +0200

    fix: cloudinary variables problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import { v2 as cloudinary } from "cloudinary";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-const app = express();
 
-app.use(express.json());
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
