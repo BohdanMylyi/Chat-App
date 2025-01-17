305cf504f20e1f44fc393201235157c56f4e8459
Bohdan Mylyi <mylyibohdan18@gmail.com>
2025-01-17 16:32:37 +0200

    add: chat container with messages

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -25,11 +25,24 @@ export const useChatStore = create((set) => ({
     set({ isMessagesLoading: true });
     try {
       const res = await axiosInstance.get(`/messages/${userId}`);
-      set({ users: res.data });
+      set({ messages: res.data });
     } catch (err) {
       toast.error(err.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (err) {
+      toast.error(err.response.data.message);
     }
   },
 
